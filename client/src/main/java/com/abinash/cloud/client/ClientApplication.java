b6c5ee3a2287aadf3c8f4ae25b3dbb764b3f6f5c
abinash1padhee <abinash1padhee@gmail.com>
2019-06-10 08:33:10 +0530

    First commit

---
+++ client/src/main/java/com/abinash/cloud/client/ClientApplication.java
@@ -0,0 +1,43 @@
+package com.abinash.cloud.client;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class ClientApplication {
+
+	@Qualifier("eurekaClient")
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String callService() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("service", false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		ResponseEntity<String> response =
+				restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+		return response.getBody();
+	}
+
+}
