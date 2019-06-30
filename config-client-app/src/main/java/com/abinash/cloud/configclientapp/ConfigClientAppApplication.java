f5c776ed9386a6dff7f570c2b2156e064016e5fd
abinash1padhee <abinash1padhee@gmail.com>
2019-06-30 10:54:50 +0530

    Added Config Client App to discover config server using discovery server.

---
+++ config-client-app/src/main/java/com/abinash/cloud/configclientapp/ConfigClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.abinash.cloud.configclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAppApplication.class, args);
+	}
+
+}
---
+++ config-client-app/src/test/java/com/abinash/cloud/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abinash.cloud.configclientapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigClientAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
