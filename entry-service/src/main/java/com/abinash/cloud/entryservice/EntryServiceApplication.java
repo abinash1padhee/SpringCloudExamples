676eb087283cd8421bd834d94596a70209dbe83e
abinash1padhee <abinash1padhee@gmail.com>
2019-07-13 14:21:44 +0530

    Added Zuul Filters with a basic request header example.

--- entry-service/src/main/java/com/abinash/cloud/entryservice/EntryServiceApplication.java
+++ entry-service/src/main/java/com/abinash/cloud/entryservice/EntryServiceApplication.java
@@ -3,6 +3,7 @@ package com.abinash.cloud.entryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,14 @@ public class EntryServiceApplication {
 		SpringApplication.run(EntryServiceApplication.class, args);
 	}
 
-	@RequestMapping
+	/*@RequestMapping
 	public String getEntry() {
 		return "Entry!";
+	}*/
+
+	@RequestMapping
+	public String hello(@RequestHeader("x-location") String location) {
+		return "Hello from " + location + "!";
 	}
 
 }
--- gateway-service/src/main/java/com/abinash/cloud/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/abinash/cloud/gatewayservice/GatewayServiceApplication.java
@@ -1,17 +1,26 @@
 package com.abinash.cloud.gatewayservice;
 
+import com.abinash.cloud.filters.AddRequestHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
+@Configuration
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public AddRequestHeaderFilter addRequestHeaderFilter() {
+		return new AddRequestHeaderFilter();
 	}
 
 }
