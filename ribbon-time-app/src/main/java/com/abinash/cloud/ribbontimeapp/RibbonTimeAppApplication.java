a11874c71b95c7b9ce14833e48f586665c140c43
abinash1padhee <abinash1padhee@gmail.com>
2019-07-14 19:27:51 +0530

    Added Ribbon Client App and a service for Client side load balancing.

---
+++ ribbon-time-app/src/main/java/com/abinash/cloud/ribbontimeapp/RibbonTimeAppApplication.java
@@ -0,0 +1,36 @@
+package com.abinash.cloud.ribbontimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import javax.inject.*;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class RibbonTimeAppApplication {
+
+	@Inject
+	private RestTemplate restTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeAppApplication.class, args);
+	}
+
+	@GetMapping
+	public String getTime() {
+		return restTemplate.getForEntity("http://time-service", String.class).getBody();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
