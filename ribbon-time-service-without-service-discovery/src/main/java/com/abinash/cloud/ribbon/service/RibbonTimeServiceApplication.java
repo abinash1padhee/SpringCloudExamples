1f635581489690ad329819e71a4bab4d34f5cf36
abinash1padhee <abinash1padhee@gmail.com>
2019-07-15 08:50:29 +0530

    Updated package structure.

---
+++ ribbon-time-service-without-service-discovery/src/main/java/com/abinash/cloud/ribbon/service/RibbonTimeServiceApplication.java
@@ -0,0 +1,28 @@
+package com.abinash.cloud.ribbon.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class RibbonTimeServiceApplication {
+
+    @Value("${server.port}")
+    private int port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonTimeServiceApplication.class, args);
+    }
+
+    @GetMapping
+    public String getTime() {
+        return "The current time is " + new Date().toString()
+                + "(answered by service running on " + port + ")";
+    }
+
+}
